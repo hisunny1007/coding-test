956866be898500fe0f2ac4200308384fb00677a2
hisunny1007 <hisunny1007@gmail.com>
2025-04-14 23:24:51 +0900

    [Bronze V] Title: A+B - 3, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,5 +1,9 @@
-T = int(input())
+import sys
 
-for _ in range(T):
-  A, B = map(int, input().split())
-  print(A + B)
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    a, b = map(int, input().split())  # .split 문자열을 공백으로 나눠서 각각의 원소에 int 먹이고 다중할당해줌
+    print(a + b)
