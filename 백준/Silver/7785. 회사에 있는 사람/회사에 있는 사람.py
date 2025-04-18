b76dedc38e670e6f1fb687f2c15a9a9a460fb5ba
hisunny1007 <hisunny1007@gmail.com>
2025-04-19 02:30:52 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 156 ms, Memory: 44756 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,18 +1,19 @@
 import sys
+
 input = sys.stdin.readline
 
 n = int(input())
-answer = set()
+now_working = set()
 
 for _ in range(n):
-  name, status = input().split()
+  name, history = input().split()
+  
+  if history == 'enter':
+    now_working.add(name)
+  elif history == "leave":
+    now_working.remove(name)
 
-  if status == "enter":
-    answer.add(name)
-  elif status == "leave":
-    answer.discard(name)
-
-answer2 = sorted(answer, reverse=True)
-
-for person in answer2:
-  print(person)
+# 역순으로
+result = sorted(now_working, reverse=True)
+# print(result) ['Askar', 'Artem'] 
+print(*result, sep="\n") # 언패킹, 개행하자
