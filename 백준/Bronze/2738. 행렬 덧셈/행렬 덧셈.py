d87e19fa76ce5f52fb604410acde94da98724de3
hisunny1007 <hisunny1007@gmail.com>
2025-04-23 19:37:33 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -2,30 +2,14 @@ import sys
 
 input = sys.stdin.readline
 
-
 n, m = map(int, input().split())
 
-# matrix1 = []
-
-# for _ in range(n):
-#   line = list(map(int, input().split()))
-#   matrix1.append(line)
-# print(matrix1)
-
-# 이걸 리스트 컴프리헨션으로
-matrix1 = [list(map(int, input().split())) for _ in range(n)]
-matrix2 = [list(map(int, input().split())) for _ in range(n)]
+matrix = [list(map(int, input().split())) for _ in range(n)]
 
 for i in range(n):
-  for j in range(m):
-    matrix1[i][j] += matrix2[i][j]
-# print(matrix1) # [[4, 4, 4], [6, 6, 6], [5, 6, 100]]
+   line = list(map(int, input().split()))
+   for j in range(m):
+      matrix[i][j] += line[j]
 
-for line in matrix1:
-    # print(line)
-    '''
-    [4, 4, 4]
-    [6, 6, 6]
-    [5, 6, 100]
-    '''
-    print(*line)
+for i in range(n):
+   print(*matrix[i])
