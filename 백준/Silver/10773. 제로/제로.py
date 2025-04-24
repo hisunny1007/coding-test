15fafb73007dcf68622e1396b963a3f18b529f3f
hisunny1007 <hisunny1007@gmail.com>
2025-04-24 11:16:00 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -3,21 +3,14 @@ import sys
 input = sys.stdin.readline
 
 k = int(input())
-
 stack = []
-answer = 0
 
 for _ in range(k):
-  num = int(input())
+  number = int(input())
 
-  if num != 0:
-    stack.append(num)
-# print(stack)
-  elif num == 0:
-    # if stack:
+  if number == 0:
     stack.pop()
-    
-# print(stack)
-for i in stack:
-  answer += i
-print(answer)      
+  else:
+    stack.append(number)
+
+print(sum(stack))
