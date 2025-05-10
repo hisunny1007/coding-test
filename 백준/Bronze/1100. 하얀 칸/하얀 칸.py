ad96d61e1363c62fb6813f1f0d6a2a626a5f52cb
hisunny1007 <hisunny1007@gmail.com>
2025-05-11 03:26:16 +0900

    [Bronze II] Title: 하얀 칸, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.py
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -1,17 +1,12 @@
-# 체스판 따로 저장해서 나중에 쓰는 게 아니기 때문에 굳이 만들 필요 없음
-# 입력 한 줄씩 읽고 바로 처리하기 때문에 입력만 잘 받음 돼
-# chesspan = [[0] * 8 for _ in range(8)]
+import sys
+input = sys.stdin.readline
 
-# 하얀칸 (0,0) (0,2), (0,4) (0,6)
-# (1,1) (1,3)...
-count = 0
+board = [input().rstrip() for _ in range(8)]
+total = 0
 
 for i in range(8):
-    chess = input()
-
     for j in range(8):
-      if (i + j) % 2 == 0: # 하얀칸
-        if chess[j] == "F":
-          count += 1
+        if i % 2 == j % 2 and board[i][j] == "F":
+            total += 1
 
-print(count)
+print(total)
