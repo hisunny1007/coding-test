27dccf1147ae4ca6fd2f419f72e9823576622e29
hisunny1007 <hisunny1007@gmail.com>
2025-05-23 23:57:05 +0900

    [Silver I] Title: 절댓값 힙, Time: 124 ms, Memory: 42144 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,19 @@
+import sys
+from heapq import heappop, heappush
+
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    x = int(input())
+
+    if x != 0:
+        heappush(heap, (abs(x), x))  # x가 0이 아니라면 (절대값, 원본)을 힙에 삽입
+        continue
+
+    if heap:
+        print(heappop(heap)[1])  # x가 0이고 힙이 비어있지 않으면 원본을 출력
+    else:
+        print(0)  # x가 0이고 힙이 비어있으면 0을 출력
