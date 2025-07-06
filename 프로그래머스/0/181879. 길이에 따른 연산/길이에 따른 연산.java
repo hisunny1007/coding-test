b385e65995129f196975d2af9feb08551d499684
hisunny1007 <hisunny1007@gmail.com>
2025-07-06 23:53:32 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        
+        if (num_list.length >= 11) {
+            int sum = 0;
+            
+            for (int num : num_list) {
+                sum += num;
+            }
+            return sum;
+        }
+        
+        int product = 1;
+        for(int num : num_list) {
+            product *= num;
+        }
+        return product;
+    }
+}
