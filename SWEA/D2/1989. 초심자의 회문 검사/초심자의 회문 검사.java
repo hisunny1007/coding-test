0aad9e074225044b036d64372accbd6a22aa6a9d
yunsun <hisunny1007@gmail.com>
2025-08-06 15:54:56 +0900

    [D2] Title: 초심자의 회문 검사, Time: 92 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -8,22 +8,42 @@ public class Solution {
 		
 		int T = sc.nextInt();
 		
+//		for(int tc = 1; tc <= T; tc++) {
+//			String word = sc.next();
+//			boolean isPal = true; // 처음에 회문으로 가정
+//			
+//			// 단어 길이
+//			int len = word.length();
+//			
+//			// 문자열 길이 절반까지만 비교
+//			
+//			for(int i = 0; i < len / 2 ; i++) {
+//				if(word.charAt(i) != word.charAt(len - 1 -i)) {
+//					isPal = false; // 하나라도 다르면 회문 아님
+//					break; // 회문 아니니까 더 비교 안 해도 됨
+//				}
+//			}
+//			int answer = isPal ? 1 : 0;
+//			System.out.println("#" + tc + " " + answer);
+//		}
+		
+		// StringBuilder 사용
 		for(int tc = 1; tc <= T; tc++) {
 			String word = sc.next();
-			boolean isPal = true; // 처음에 회문으로 가정
 			
-			// 단어 길이
-			int len = word.length();
+			StringBuilder sb = new StringBuilder(word);
 			
-			// 문자열 길이 절반까지만 비교
+			// 쉽게 출력하기 위해서 anwer 변수 저장 후 출력
+			int answer = 0;
 			
-			for(int i = 0; i < len / 2 ; i++) {
-				if(word.charAt(i) != word.charAt(len - 1 -i)) {
-					isPal = false; // 하나라도 다르면 회문 아님
-					break; // 회문 아니니까 더 비교 안 해도 됨
-				}
+			if(word.equals(sb.reverse().toString())) {
+//				System.out.println("#" + tc + " " + 1);
+				answer = 1;
+			} else {
+//				System.out.println("#" + tc + " " + 0);
+				answer = 0;
 			}
-			int answer = isPal ? 1 : 0;
+			
 			System.out.println("#" + tc + " " + answer);
 		}
 		
