661bb7a7e0566523cfc88d34a18ba74401aa1b70
yunsun <hisunny1007@gmail.com>
2025-08-12 16:40:36 +0900

    [D3] Title: 준홍이의 카드놀이, Time: 234 ms, Memory: 31,232 KB -BaekjoonHub

--- SWEA/D3/7102. 준홍이의 카드놀이/준홍이의 카드놀이.java
+++ SWEA/D3/7102. 준홍이의 카드놀이/준홍이의 카드놀이.java
@@ -1,11 +1,15 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		// 2시 45분
+		// 3시 50분
+//		File file = new File("./src/SWEA_7102_준홍이의_카드놀이/sample_input.txt");
+//		Scanner sc = new Scanner(file);
 		Scanner sc = new Scanner(System.in);
 		
 		int t = sc.nextInt();
@@ -15,43 +19,60 @@ public class Solution {
 			int n = sc.nextInt();
 			int m = sc.nextInt();
 			
-			// 합 배열 만들어줌 // 최대 N + M까지 나올 수 있음
-			// n = 6 / 1 2 3 4 5 6
-			// n = 4 / 1 2 3 4
-			int[] cntArr = new int[n+ m + 1]; // 0번부터이니까 + 1 해줘야 함
-//			System.out.println(Arrays.toString(cntArr));
-//			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+			int sum = 0;
+			
+			// 해시맵 활용 (키밸류값 구하기 위해)
+			Map<Integer, Integer> map = new HashMap<>();
+			
+			int count = 0;
 
-			// 인덱스 활용함
-			// 인덱스 값(=카드 번호)이 몇 번 나왔는지!
 			for(int i = 1; i < n + 1; i++) {
 				for(int j = 1; j < m + 1; j++) {
-					cntArr[i + j]++;
+					// count이 반복문 밖에 있으니까 .. 누적합이 된다..
+					// 키값(sum)에 관계없이 밸류(count)가 누적되고 있음 ;;
+					sum = i + j;
+//					count++; // 모든 쌍에서 ++하고 있음 .. ; 특정 sum일 때 count해야돼
+//					// sum을 키값으로, count를 밸류값으로
+//					map.put(sum, count);
+					
+					// (원래 있던 특정 키 값의 밸류값에) 더해야 한다!!!!!!!!
+					// 키가 있으면 ... 거기에 있던 밸류값에 +1
+					// 키가 없으면 새롭게 만든 키를 넣어줘.. 그리고 밸류값을 1 (새로운 거니까)
+					
+					// sum이 있으면 +1, 없으면 0
+					map.put(sum, map.getOrDefault(sum, 0) + 1);
+					
 				}
 			}
+//			System.out.println(map);
+//			{2=1, 3=2, 4=3, 5=4, 6=5, 7=6, 8=5, 9=4, 10=3, 11=2, 12=1}
+//			{2=1, 3=2, 4=3, 5=4, 6=4, 7=4, 8=3, 9=2, 10=1}
 			
-			// 최대값 구해야 함( 등장할 확률이 가장 높은 숫자)
+			// 등장할 확률이 가장 높은 숫자 구하기
+			// value값이 가장 큰 key값 출력!
 			int max = Integer.MIN_VALUE;
 			
-			for(int i = 0; i < cntArr.length; i++) {
-				if(cntArr[i] > max) {
-					max = cntArr[i];
+//			System.out.println(map.values());
+//			[1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
+//			[1, 2, 3, 4, 4, 4, 3, 2, 1]
+			
+			for(int v : map.values()) {
+				if ( v > max ) {
+					max = v;
 				}
 			}
 			
-			// 결과 출력
+			// max값과 같은 value를 가진 key 출력
 			System.out.print("#" + tc + " ");
-			// 만약에 여러 개라면 오름차순 정렬
-			for(int i = 0; i < cntArr.length; i++) {
-				if(cntArr[i] == max) {
-					System.out.print(i + " ");
-				}
+			for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			    if (entry.getValue() == max) {
+			        System.out.print(entry.getKey() + " ");
+			    }
 			}
-			System.out.println(); // 테케마다 줄 바꿈
-						
-		} // 테스트케이스 e
-
+			System.out.println();
+			
+		}
+		
 		sc.close();
 		
 	} // 메인 e
