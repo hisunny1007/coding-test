c5763bcc4bf4ae451e6e840b0dbc88510b0e78da
yunsun <hisunny1007@gmail.com>
2025-08-17 21:13:37 +0900

    [D3] Title: 공과 잡초, Time: 109 ms, Memory: 28,672 KB -BaekjoonHub

--- SWEA/D3/14555. 공과 잡초/공과 잡초.java
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) throws FileNotFoundException {
-		
-//		File file = new File("./src/SWEA_14555_공과_잡초/sample_input.txt");
-//		Scanner sc = new Scanner(file);
+
 		Scanner sc = new Scanner(System.in);
 		
 		int t = sc.nextInt();
@@ -18,24 +16,16 @@ public class Solution {
 			
 			// 일단 문자 하나씩 뜯기
 			for(int i = 0 ; i < s.length() - 1; i++) {
-				char c = s.charAt(i);
-				char c1 = s.charAt(i+1);
-
-		        // (|  경우
-				if (c == '(' && c1 == '|') {
-						count++;
-				}
 				
-		        // |)  경우
-				if (c == '|' && c1 == ')') {
+				// substring : 문자열에서 일부 구간을 잘라서 새로운 문자열로 반환
+				// substring(시작인덱스 포함o, 끝 인덱스 포함x)
+				// 한 글자만 뽑고 싶으면 substring(i, i+1)
+				// 연속된 패턴 체크할 때 유용
+				String two = s.substring(i, i+2); // 두 글자씩 뽑기
+				
+				if(two.equals("()") || two.equals("(|") || two.equals("|)")) {
 					count++;
 				}
-				
-		        // ()  경우
-		        if (c == '(' && c1 == ')') {
-		            count++;
-		        }
-				
 			}// 문자열 e
 			
 			System.out.println("#" + tc + " " + count);
