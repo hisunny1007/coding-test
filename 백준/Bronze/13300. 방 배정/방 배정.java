0343f6d7fb883a6a69aec8df185cb2d71783f1ff
yunsun <hisunny1007@gmail.com>
2025-08-24 01:27:57 +0900

    [Bronze II] Title: 방 배정, Time: 232 ms, Memory: 21748 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -23,7 +23,6 @@ public class Main {
 		//5
 		//6
 	
-		int room = 0;
 		
 		for(int t = 0; t < n; t++) {
 			// (gender, grade) 좌표
@@ -32,17 +31,24 @@ public class Main {
 
 			// 굳이 행렬 돌면서 다 확인할 필요 없음
 			arr[grade][gender]++;
-		}
+		}// 값 입력받기
 		
+		int room = 0;
 		
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 1; i < arr.length; i++) { // 1학년부터
 			for(int j = 0; j < arr[0].length; j++) {
 				if(arr[i][j] != 0) {
 					if(arr[i][j] % k == 0) {
 						room += arr[i][j] / k;
 					} else {
-						room += arr[i][j] / k;
-						room += arr[i][j] % k;
+						// 예를 들어 arr[i][j] = 7, k = 2이면
+						room += arr[i][j] / k; // 몫도 더하고
+//						room += arr[i][j] % k; // 나머지도 더하기  // 나머지 값 전체를 더하는 게 아니라 
+						// 나머지가 0이 아니면 1 더하는 거임!!
+						// 값이 11이고 k = 3이면
+						// 몫 3 (방3개) + 나머지 2 (방1개)
+						room ++; // 나머지 0이 아니니까 1 더하기
+						// "몫 + (나머지가 있으면 1)"
 					}
 				}
 			}//
