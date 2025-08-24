445cf7f0a9556fe63c5dbe485e77d36a57f3fe98
yunsun <hisunny1007@gmail.com>
2025-08-24 15:07:48 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 168 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,24 +17,28 @@ public class Main {
 		// 9 난쟁이 키 합 140
 				
 		// 전체를 돌면서 두개씩 확인해. 그 두 개의 합을 뺴서 100이 나오면 끝
-		outer: for(int i = 0; i < 8; i++) {
+		
+		for(int i = 0; i < 8; i++) {
 			for(int j = i + 1; j < 9; j++) {// i 다음부터 끝까지
-		        int heightSum = dwarf[i] + dwarf[j];
+		        
+				int heightSum = dwarf[i] + dwarf[j];
+				
 		        if (sum - heightSum == 100) {
 		        	dwarf[i] = 0;
 		        	dwarf[j] = 0;
-		        	break outer; // 100인거 찾으면 바로 끝내기! (가능한 정답이 여러 가지인 경우에는 아무거나 출력한다.)
-				}
+		        	
+		        	Arrays.sort(dwarf); // 0은 앞으로 나옴 [0, 0, 7, 8, 10, 13, 19, 20, 23]
+		        	for(int k = 2; k < 9; k++) {
+		        		System.out.println(dwarf[k]);			
+		        	}
 		        
-			}
-		}
-	
-		Arrays.sort(dwarf); // 0은 앞으로 나옴 [0, 0, 7, 8, 10, 13, 19, 20, 23]
-
-		for(int i = 2; i < 9; i++) {
-			System.out.println(dwarf[i]);			
-		}
-		 
+		        	return; // 바로 종료함
+		        }
+		        
+			}//
+		}//
+		
 		sc.close();
-	}//
-}//
+		
+	}// main e
+}// class e
