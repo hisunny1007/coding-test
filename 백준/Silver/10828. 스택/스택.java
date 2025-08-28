27845bf19186c7cd16c5c20d5c16b60340f69532
yunsun <hisunny1007@gmail.com>
2025-08-29 01:51:44 +0900

    [Silver IV] Title: 스택, Time: 480 ms, Memory: 38068 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		int n = sc.nextInt();
+        sc.nextLine(); // 남아 있는 줄바꿈(\n) 제거
+		
+		
+		for(int i = 0; i < n; i++) {
+			// 한 줄로 받고 공백에서 자르기
+			String[] line = sc.nextLine().split(" ");//
+//			System.out.println(Arrays.toString(line)); [push, 1]
+			
+			String order = line[0];
+//			int num = Integer.parseInt(line[1]); // 숫자가 없는 push 제외 연산들은 ArrayIndexOutofBoundsException
+			
+			// push, pop, size, empty, top일 경우
+			// line의 원소가 두 개일때 조심해야 함!! 그래서 그떄 경우만 체크
+			// 문자열이니까 == 못쓰고 .equals()
+			if(order.equals("push")) {
+				int num = Integer.parseInt(line[1]);
+				stack.push(num);
+			} else if (order.equals("pop")) {
+				if (stack.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(stack.pop());
+				}
+			} else if (order.equals("size")) {
+				System.out.println(stack.size());
+			} else if (order.equals("empty")) {
+				if (stack.isEmpty()) {
+					System.out.println(1);
+				} else {
+					System.out.println(0);
+				}
+			} else if (order.equals("top")) {
+				if (stack.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(stack.peek());
+				}
+			}
+		
+		}
+		
+		sc.close();
+	}//main
+}// class
