452334c796ee57c77f4a2ae9a52fb6b13216b05c
yunsun <hisunny1007@gmail.com>
2025-08-31 03:21:31 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 104 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -9,67 +10,81 @@ import java.util.Set;
 
 public class Solution {
 	public static void main(String[] args) throws FileNotFoundException {
-	
+		
 		Scanner sc = new Scanner(System.in);
-						
+		
 		int t = sc.nextInt();
 		
 		for(int tc = 1; tc <= t; tc++) {
-			int n = sc.nextInt(); // 숫자 개수(4의 배수)
-			int k = sc.nextInt(); // k번째 큰 수
+			int n = sc.nextInt();
+			int k = sc.nextInt();
 			
-			String str = sc.next(); // 입력 문자열 (16진수)
-						
-			// 중복 제거해야하니까 set쓰자
-			Set<Integer> set = new HashSet<>();
-            
-			// 아래 과정을 n/4만큼 반복 
+			String str = sc.next();
 			
-			// 1. 현재 문자열을 n/4만큼 자르기 (4개의 문자열)
-			// 2. 4개의 문자열 각각을 16진수 -> 10진수로 변환
-			// 3. 10진수 숫자를 set에 넣기 (자동으로 중복 제거)
-			// 4. 문자열을 한 칸 회전(맨 뒤-> 맨 앞)
+			// 일단 저 입력받은 str을 4묶음으로 만들어야 함 (4개의 변)
 			
-			// n/4만큼 반복 다 끝나면 set을 정렬하고 k번째 값 구하기
+			// 중복 제거위해 set이용
+			Set<String> set = new HashSet<>(); // 문자열
 			
-			int len = n / 4; // 한 변의 길이
-
-			// 회전은 len번만 함
-			for(int rotate = 0; rotate < len; rotate++) {
+			int len = n / 4; // 한 묶음 안의 원소 개수
+			
+			
+			// 반복은 len만큼
+			for(int r = 0; r < len; r++) {
 				
-				// 입력 문자열을 4개의 문자열로 만들고 10진수 변환해서 set에 넣기
-				for(int i = 0; i < str.length(); i += len) {
-					String part = str.substring(i, i + len); // n/4로 나눈 한 묶음 (4개의 문자열)
-//				System.out.println(part); // 1B3 B3B 81F 75E	
+				
+				// 1. 한 묶음들을 set에 넣기
+				//1B3, B3B, 81F, 75E
+				// 인덱스는 0-2 / 3-5/ 6-8/ 9-11
+				for(int i = 0 ; i < str.length(); i += len) {
+					String word = str.substring(i, i+len); // 4묶음으로 만들기 (한 묶음 안의 원소 개수는 len)
+//				System.out.println(word);
 					
-					// 한 묶음을 16진수 -> 10진수로 변환
-					int number = Integer.parseInt(part, 16);
-					// 변환한 10빈수를 set에 넣기
-					set.add(number);
+					// 이렇게 나온 한 묶음들을 set에 넣기
+					set.add(word);
 				}
 				
-				// 원본 문자열 회전 (맨 마지막 문자가 맨 앞으로 이동)
-				// 문자열을 붙여 만들기( 맨 마지막 문자 + 처음부터 맨 마지막-1)
+				// 2. 맨 뒤 -> 맨 앞으로 회전
+				// 문자열 덮어씌우기
+				// 결국에 문자열은 맨뒤값 + 맨앞~맨뒤-1
+				// substring(start, end) end는 포함안되니까
 				str = str.charAt(str.length() - 1) + str.substring(0, str.length() - 1);
+//				System.out.println(str);
+//				1B3B3B81F75E
+//				E1B3B3B81F75
+//				5E1B3B3B81F7
+//				75E1B3B3B81F
+
 			}
 			
-			// 정렬
-			// set을 정렬하려면 List로 변환한 뒤 정렬해야 함.
-			// set을 리스트에 넣기
-			List<Integer> list = new ArrayList<>(set);
+			// n/4만큼 회전해서 나온 모든 묶음들이 set에 저장됨(중복은 알아서 제거)
+//			System.out.println(set);
+			// [B3B, 5E1, 1B3, 1F7, 3B3, F75, 75E, E1B, 3B8, B81, 81F]
 			
-			// 내림차순 정렬
+			// 3. set에 있는 모든 원소 16진수 -> 10진수 변환
+			// 4. 리스트에 저장 (set은 정렬안되므로 리스트로 바꾸기)
+			List<Integer> list = new ArrayList<>();
+			
+			for(String s : set) {
+				int num = Integer.parseInt(s, 16);
+				list.add(num);
+			}
+			
+//			System.out.println(list); // [2875, 1505, 435, 503, 947, 3957, 1886, 3611, 952, 2945, 2079]
+
+			
+			// 5. 정렬하고 k번쨰로 큰 수 찾기
+			// set은 정렬안되므로 리스트로 바꾸기
+			// 내림차순
 			Collections.sort(list, Collections.reverseOrder());
-//			System.out.println(list);
-			
-			// k번째 큰 수 인덱스: k - 1
-			int answer = list.get(k - 1);
-			
-			System.out.println("#" + tc + " " + answer);
-			
-		}// 테케 e
+//			System.out.println(list); // [3957, 3611, 2945, 2875, 2079, 1886, 1505, 952, 947, 503, 435]
+
+			System.out.println("#" + tc + " " + list.get(k - 1));
 		
-		
+			
+		}//tc
+
+	
 		sc.close();
-	}//main
-}//class
+	}//
+}//
