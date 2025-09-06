21922db0accc64aac03e9ab1c1f1f8f192c759f6
yunsun <hisunny1007@gmail.com>
2025-09-06 19:55:45 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.11 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/README.md
+++ 프로그래머스/0/181901. 배열 만들기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 1 - 181901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181901?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181901) 
 
 ### 성능 요약
 
-메모리: 89.9 MB, 시간: 0.07 ms
+메모리: 82 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 05일 11:31:32
+2025년 09월 06일 19:55:41
 
 ### 문제 설명
 
--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] solution(int n, int k) {
-    
-        int count = n / k; // k배수 개수
+        int len = n / k;
+        int[] answer = new int[len];
         
-        int[] answer = new int[count];
-        // answer = [0, 0, 0]
-        
-        for(int i = 1; i <= count; i++) {
-             answer[i-1] = i * k;
+        int index = 0;
+        for(int i = k; i <= n; i += k) {
+            answer[index] = i; 
+            index++;
         }
-             
+        
         return answer;
     }
 }
