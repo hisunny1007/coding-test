ad82e68b4bbcbe299e0a9978908d881b8326cdce
yunsun <hisunny1007@gmail.com>
2025-09-07 19:43:07 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,22 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int n) {
         
-        List<Integer> list = new ArrayList<>();
-        for(int i = 1; i <= n; i++) {
-            if ( i % 2 != 0) {
-                list.add(i);
-            }
-        }
-        // return list; List<Integer> cannot be converted to int[]
+        // 홀수 개수 알아야 함
+        int size = (n + 1) / 2;
         
-        // ArrayList를 int[] 로 변환
-        int[] answer = new int[list.size()];
+        int[] answer = new int[size];
         
-        for(int i = 0; i < list.size(); i ++) {
-            answer[i] = list.get(i);
+        int index = 0;
+        // for(int i = 1; i <= n; i++) {
+        //     if(i % 2 != 0) {
+        //         answer[index++] = i;
+        //     }
+        // }
+        for(int i = 1; i <= n; i+=2) {
+            answer[index++] = i;
         }
         
         return answer;
-        
     }
 }
