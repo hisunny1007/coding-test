74846bf61ce85214aef980f6b001228913a8fcf9
yunsun <hisunny1007@gmail.com>
2025-09-15 02:39:05 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 117 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class Solution {
@@ -24,11 +23,9 @@ public class Solution {
 						startR = i;
 						startC = j;
 					}
-					
 				}
 			}
-			
-			
+
 			boolean[][] visited = new boolean[n][n];
 			
 			int[] dr = {-1, 1, 0, 0};
@@ -37,12 +34,11 @@ public class Solution {
 			// 정답 변수 (도달 가능하면 1로 갱신)
 			int answer = 0;
 			
-			// bfs를 위한 큐 만들기
-			// 정수배열[r,c]를 요소로 하는 큐를 만들고
+			// bfs를 위한 큐 만들기 - 정수배열[r,c]를 요소로 하는 큐를 만들고
 			Queue<int[]> queue = new ArrayDeque<>();
 			
-			// 시작좌표(0, 0)방문체크하고 넣기
-			visited[0][0] = true;
+			// 시작좌표 방문체크하고 큐에 넣기
+			visited[startR][startC] = true; // 큐에 넣을 때 방문 체크
 			queue.add(new int[] {startR, startC});
 			
 			while(!queue.isEmpty()) {
@@ -51,34 +47,26 @@ public class Solution {
 				int r = curr[0];
 				int c = curr[1];
 				
-				visited[r][c] = true;
-				
 				// 도착점이면 도달 가능 (1 가능함)
-				if(maze[r][c] == 3) {
+				if(maze[r][c] == 3) { // 도착점 확인
 					answer = 1;
 					break;
 				}
-				
-				
+
 				// 4방향 탐색
 				for(int i = 0; i < 4; i++) {
 					int nr = r + dr[i];
 					int nc = c + dc[i];
 					
-					// 범위를 벗어나면 넘어가
-					if(nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
-					// 방문했거나 벽이면 넘어가
-					if(visited[nr][nc] || maze[nr][nc] == 1) continue;
+			        if(nr<0 || nr>=n || nc<0 || nc>=n) continue; // 범위 체크
+			        if(visited[nr][nc] || maze[nr][nc] == 1) continue; // 방문 또는 벽 체크
 					
-					visited[nr][nc] = true;
-					queue.add(new int[] {nr, nc});
-					
+					visited[nr][nc] = true; // 큐에 넣을 때 방문 체크
+					queue.add(new int[] {nr, nc}); // 큐에 넣기
 				}
 			}
-
 			System.out.println("#" + tc + " " + answer);
 		}// tc
-		
 		sc.close();
 	}//
 }//
