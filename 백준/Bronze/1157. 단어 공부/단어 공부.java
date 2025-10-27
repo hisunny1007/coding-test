b7f050cfd355e476c7ab1e7f98cfa3d56e25f8cb
yunsun <hisunny1007@gmail.com>
2025-10-27 14:19:21 +0900

    [Bronze I] Title: 단어 공부, Time: 416 ms, Memory: 29004 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,46 +6,39 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		// 대소문자 구분 안함
+		// 대소문자 구분 안 함
 		String words = sc.next().toUpperCase();
-				
-		int[] cnt = new int[26];
 		
-		
+		int[] cnt = new int[26]; // 알파벳 개수만큼 배열 만들기
 		
 		// 1. 글자 하나씩 꺼내서 개수 세기
 		for(int i = 0; i < words.length(); i++) {
 			char ch = words.charAt(i);
-//			System.out.println(ch - 'A'); 
+//			System.out.println(ch); // 문자 그자체
+//			System.out.println(ch - 'A'); // 아스키코드
 			
-			cnt[ch - 'A']++; // 문자 -> 숫자 
+			cnt[ch - 'A']++; // 문자 -> 숫자
 		}
 		
 		
 //		System.out.println(Arrays.toString(cnt));
-		
-		// 2. 가장 많이 나온 횟수랑 그 문자 찾기
-		int max = Integer.MIN_VALUE;
+//		[0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0]
+
+		// 2. cnt 배열에서 가장 값이 큰 숫자에 해당하는 문자 찾기
+		int max = 0;
 		char answer = '?';
-		
 		
 		for(int i = 0; i < 26; i++) {
 			if(cnt[i] > max) {
-				max = cnt[i];
+				max = cnt[i]; 
 				answer = (char) (i + 'A'); // 숫자 -> 문자
 			} else if(cnt[i] == max) {
-				answer = '?'; // 같은 알파벳 여러 개면 그냥 다시 물음표
+				answer = '?';
 			}
 		}
+		
 		System.out.println(answer);
 
 		sc.close();
-		
 	}
 }
-
-
-// ch - 'A' : 문자를 숫자 인덱스로 바꾸는 공식
-// 'A' → 배열의 0번 인덱스
-// 'B' → 배열의 1번 인덱스
-// 'C' → 배열의 2번 인덱스
