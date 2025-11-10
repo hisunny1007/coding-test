00ad8d1c8a9c54c88428cb05977ef5605b9b41b7
yunsun <hisunny1007@gmail.com>
2025-11-10 22:22:55 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 180 ms, Memory: 18228 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,40 +1,34 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
 		String str = sc.next();
 		
-		//알파벳 배열
-		// -1 로 초기화
+		// 알파벳 배열
 		int[] arr = new int[26];
+		// 초기값은 -1로 정함
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = -1;
 		}
 		
-		
 		for(int i = 0; i < str.length(); i++) {
 			char ch = str.charAt(i);
-			int idx = ch - 'a'; 
-//			System.out.println(ch + " " + idx);
 			
-			// 알파벳 배열 돌면서 값 변경
-			for(int j = 0; j < arr.length; j++) {
-				// 이미 한번 나온 값이라면 무시함
-				if(arr[idx] != -1) {
-					continue;
-				}
-				arr[idx] = i;
-				
+			int idx = ch - 'a';
+//			System.out.println(idx);
+			
+			// 아직 한 번도 등장하지 않은 경우만 기록함
+			if(arr[idx] == -1) {
+				arr[idx] = i;		
 			}
 		}
 		
 		for(int num : arr) {
 			System.out.print(num + " ");
 		}
+		
 		sc.close();
 	}
 }
