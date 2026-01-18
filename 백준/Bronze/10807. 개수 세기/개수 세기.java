9bc21776b45a445cad76ef7665c2b377a357ddf4
yunsun <hisunny1007@gmail.com>
2026-01-18 19:45:22 +0900

    [Bronze V] Title: 개수 세기, Time: 180 ms, Memory: 17796 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,31 +1,27 @@
 import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         
-        // 정수 개수 n
         int n = sc.nextInt();
-        // 입력 배열
-        int[] num = new int[n];
-
+        int[] numbers = new int[n];
         for(int i = 0; i < n; i++) {
-            num[i] = sc.nextInt();
+            numbers[i] = sc.nextInt();
         }
-
-        // 찾으려고 하는 정수 v
-        int v = sc.nextInt();
-        // 개수 변수
-        int cnt = 0;
-
-        for(int i = 0; i < num.length; i++) {
-            if(num[i] == v)
-                cnt++;
-        }
-
-        System.out.println(cnt);
         
- 
+        int v = sc.nextInt();
+        
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(numbers[i] == v) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+        
+        sc.close();
     }
 }
