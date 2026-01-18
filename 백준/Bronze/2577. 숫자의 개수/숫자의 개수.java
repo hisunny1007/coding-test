eb626092fd52003027306a4691c4490eff4d896e
yunsun <hisunny1007@gmail.com>
2026-01-18 21:29:49 +0900

    [Bronze II] Title: 숫자의 개수, Time: 172 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,39 +1,36 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		
-		int multiple = 1;
-		
-		// 세 개의 숫자를 입력받아 곱셈
-		for(int i = 0; i < 3; i++) {
-			int num = sc.nextInt();
-			multiple *= num;
-		}
-				
-		// 곱셈 결과를 문자열로 변환
-		String str = Integer.toString(multiple);
-		
-	     // 숫자 카운트 배열 초기화 (0부터 9까지)
-        int[] count = new int[10];  // 0~9까지 숫자를 카운트할 배열
-        
-        // 각 자리 숫자를 확인하고 카운팅
-        for(int i = 0; i < str.length(); i++) {
-//			count[arr[i]]++; // arr[i]은 char 타입인데 count배열 인덱스 사용하려면 숫자로 변환해야 함
-            int digit = str.charAt(i) - '0';  // 문자 '0'을 빼서 숫자 값으로 변환
-            count[digit]++;  // 해당 숫자 인덱스의 카운트 증가
-        }
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
 
-		// 출력하기
-        for(int i = 0; i < count.length; i++) {
-        	System.out.println(count[i]);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    int number = a * b * c;
+
+    String word = Integer.toString(number); // int -> String 변환
+    int[] answer = new int[10]; // 0 ~ 9 카운팅 배열
+
+    for(int i = 0; i < word.length(); i++) {
+      
+      char ch = word.charAt(i);
+      int num = ch - '0'; // char -> int 형 변환
+
+      // 0~9에서 비교해야 함
+      for(int j = 0; j <= 9; j++) {
+        if(num == j) {
+          answer[j]++;
         }
-		
-		
-		sc.close();
-	}//
-}//
+      }
+    }
+    
+    // 정답 출력
+    for(int ans : answer) {
+      System.out.println(ans);
+    }
+    sc.close();
+  }
+}
