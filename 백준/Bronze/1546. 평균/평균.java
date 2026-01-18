b78413d8808f89414544a4df29766cc9a740cd74
yunsun <hisunny1007@gmail.com>
2026-01-18 19:39:21 +0900

    [Bronze I] Title: 평균, Time: 208 ms, Memory: 18688 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,54 +1,38 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt(); // 과목 개수 n
-		double[] score = new double[n]; // 기존 점수 배열
-		// double로 해야함(소수점 계산 위해)
+public class Main{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
 
-
-		// 기존 점수 중 최댓값
-		double m = 0;
-		
-		// 합
-		double sum = 0;
-		
-		
-		// 기존 점수 배열에서 최댓값 구하기
-		for(int i = 0; i < n; i++) {
-			score[i] = sc.nextInt();
-		
-			if(score[i] > m) {
-				m = score[i];
-			}
-		}//
-
-		
-		// 새로운 점수 변환 후 합계
-		for(int i = 0; i < n; i++) {
-			score[i] = (score[i] / m) * 100;
-			
-			sum += score[i];
-		}
-		
-		// 평균 구하기
-		double avg = sum / n;
-		
-		System.out.println(avg);
-
-		sc.close();
-	}
+        // 성적 배열 만들고 입력
+        double[] scores = new double[n];
+        for(int i = 0; i < n; i++) {
+            scores[i] = sc.nextInt();            
+        }
+        
+        // 성적 배열에서 최대값 구하기
+        double highScore = 0;
+        for(int i = 0; i < n; i++) {
+            if(scores[i] > highScore) highScore = scores[i];
+        }
+        
+        // 새로운 평균 구하기
+        double sum = 0;
+        for(int i = 0; i < n; i++) {
+            scores[i] = (scores[i]/highScore ) * 100;
+            sum += scores[i];
+        }
+        
+        double average = (sum / n);
+        System.out.println(average);
+        
+        
+       
+        
+        
+        
+    }
 }
-//
-//과목의 개수 n
-//현재 성적에서 최댓값을 구하기 m
-//모든 점수를 점수/m*100으로 바꾸고 평균 구하기
-//
-//1. 성적 배열 만들기 (크기 n)
-//2. 성적 배열에서 최댓값 m 구하기
-//3. 성적 배열에서 하나식 뽑아서 새롭게 점수 구하고
-//4. 평균 구하기 // 소수점도 있으니까 double로 
