435c0a104527b21f2d4af3cab52b359aeffc9232
yunsun <hisunny1007@gmail.com>
2026-01-23 00:06:06 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(String s) {
+        
+        if(s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        
+        // 숫자로만 구성돼있는지
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) < '0' || s.charAt(i) > '9') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
