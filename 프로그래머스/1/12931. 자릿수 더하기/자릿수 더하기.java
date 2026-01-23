f96d2eca467577025442d8dc4855401eba4de8d4
yunsun <hisunny1007@gmail.com>
2026-01-23 12:46:41 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.17 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        
+        int answer = 0;
+        
+        String[] arr = String.valueOf(n).split("");
+        
+        for(String s : arr) {
+            answer += Integer.parseInt(s);
+        }
+        
+        return answer;
+    }
+}
