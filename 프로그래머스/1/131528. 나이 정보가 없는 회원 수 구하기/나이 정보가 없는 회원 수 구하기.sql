49a178bc3c5ac004eb0e7220981a06fe45b9d234
yunsun <hisunny1007@gmail.com>
2026-01-26 00:57:46 +0900

    feat: CASE 사용 다른 풀이 추가 - SQL query for counting users with NULL age
    Refactor SQL query to use CASE for counting users without age information.

--- 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
+++ 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
@@ -1,4 +1,15 @@
 -- 코드를 입력하세요
-SELECT COUNT(*) AS USERS
-FROM USER_INFO
-WHERE AGE IS NULL
+# SELECT COUNT(*) AS USERS
+# FROM USER_INFO
+# WHERE AGE IS NULL
+
+# CASE로 푸는 방법 연습하기
+# 나이 없으면 1 -> 1만 더해서 개수 계산
+SELECT 
+    SUM(
+        CASE 
+            WHEN AGE IS NULL THEN 1 
+            ELSE 0 
+        END
+    ) AS USERS
+FROM USER_INFO;
